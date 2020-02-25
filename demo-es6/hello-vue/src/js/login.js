e2d90e5b1354dd829e6559978ad9dccb861a953f
IamZY <562018301@qq.com>
2020-02-25 17:00:51 +0800

    Vue

---
+++ demo-es6/hello-vue/src/js/login.js
@@ -0,0 +1,11 @@
+// 组件内得template只能由一个跟标签
+const loginForm = {
+    template: `  
+        <div>
+            <h1>登录页</h1>
+            用户名：<input type="text"> <br>
+            密&emsp;码：<input type="text"> <br>
+            <input type="button" value="登陆">
+        </div>
+    `
+}
---
+++ demo-es6/hello-vue/src/js/register.js
@@ -0,0 +1,10 @@
+const registerForm = {
+    template: `  
+        <div>
+            <h1>注册页</h1>
+            用户名：<input type="text"> <br>
+            密&emsp;码：<input type="text">  <br>
+            <input type="button" value="注册">
+        </div>
+    `
+}
